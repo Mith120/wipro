ee347f8734acfa71fe82a8e3f5cdf0d2c70ee059
Mitharshana <166990511+Mith120@users.noreply.github.com>
2025-07-16 14:01:46 +0530

    Create hands-on2 class and object.java

---
+++ hands-on2 class and object.java
@@ -0,0 +1,20 @@
+link: https://onlinegdb.com/Mx5zgpa1v
+
+
+import java.util.*;
+public class Main
+{
+    int powerInt(int num1,int num2)
+    {
+        return (int)Math.pow(num1,num2);
+    }
+    double powerDouble(double num1,double num2)
+    {
+        return Math.pow(num1,num2);
+    }
+	public static void main(String[] args) {
+	    Main m=new Main();
+		System.out.println(m.powerInt(2,2));
+		System.out.println(m.powerDouble(2.0,2.0));
+	}
+}
